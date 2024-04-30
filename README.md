6cb89bdd003512949e50eadd8dc07ad0615eeb0e
Po <chenjunfeng@ethstorage.io>
2024-04-30 00:37:05 -0700

    Add notes and update zkWasm branch

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Prove op-geth with smoke test 
+System requirements: ubuntu 22.04, cuda 12.2, one Nvidia 4090 GPU, CPU with 32 cores, 528 Gb RAM
 ```
 git submodule update --init
 bash test_cont.sh
 ```
+
+## Notes
+- About 2.2B WASM instructions for smoke test
+- 1055 segments are generated for zkWasm's continuation proving
+    - (generating witness costs ~6 minites)
+    - proving 1 segment cost ~17 second, total proving time is ~6 hours
--- test_cont.sh
+++ test_cont.sh
@@ -4,20 +4,20 @@ export CUDA_VISIBLE_DEVICES=0 # for enable only one device
 set -e
 set -x
 
-PWD=$PWD
+WORKING_DIR=$PWD
 
 # build zkWasm
 cd zkWasm
-cargo build --release --features continuation,cuda
-ZKWASM=$PWD/zkWasm/target/release/delphinus-cli
+cargo build --release --features continuation,perf
+ZKWASM=$WORKING_DIR/zkWasm/target/release/zkwasm-cli
 # rm -rf output/*.data
 
-WASM_FILE=$PWD/image/op-program-client-test.wasm
-PRIVATE_FILE=$PWD/image/preimages-test.bin
+WASM_FILE=$WORKING_DIR/image/op-program-client-test.wasm
+PRIVATE_FILE=$WORKING_DIR/image/preimages-test.bin
 
 # Single test
-RUST_LOG=info $ZKWASM --params ${PWD}/params zkmain setup -k 23
+RUST_LOG=info $ZKWASM --params ${WORKING_DIR}/params zkmain setup -k 23
 
-RUST_LOG=info $ZKWASM --params ${PWD}/params zkmain prove --file --output ./output --wasm $WASM_FILE  --private $PRIVATE_FILE:file
+RUST_LOG=info $ZKWASM --params ${WORKING_DIR}/params zkmain prove --output ./output --wasm $WASM_FILE  --private $PRIVATE_FILE:file --file
 
 # $BATCHER --param $pwd/params --output $pwd/output batch -k 22 --challenge sha --info  $pwd/output/$NAME.loadinfo.json --name ${NAME}_agg --commits $CONT_BATCH_INFO --cont
